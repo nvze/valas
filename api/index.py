4f43352445dd3b636a848fe656ad96d7def3eebf
nvze <107895716+nvze@users.noreply.github.com>
2026-09-26 02:04:52 +0700

    update index.py

--- api/index.py
+++ api/index.py
@@ -8,21 +8,18 @@ def get_harga(pair):
     pair_upper = pair.upper()
     
     # Menyesuaikan simbol dengan format Yahoo Finance
-    if pair_upper == 'EURUSD':
-        yahoo_symbol = 'EURUSD=X'
-        spread = 0.0002
+    if pair_upper == 'USDIDR':
+        yahoo_symbol = 'IDR=X'
+        spread = 15.0  # Spread simulasi 15 Rupiah
     elif pair_upper == 'XAUUSD':
-        yahoo_symbol = 'XAUUSD=X'  # Simbol Emas Spot di Yahoo
+        yahoo_symbol = 'XAUUSD=X'
         spread = 0.50
     else:
-        # Jika Anda menambahkan GBPUSD, USDJPY, dll
         yahoo_symbol = f'{pair_upper}=X'
         spread = 0.0005
 
-    # Endpoint rahasia Yahoo Finance Chart API (Gratis & Tanpa Key)
     url = f'https://query1.finance.yahoo.com/v8/finance/chart/{yahoo_symbol}?region=US&lang=en-US'
     
-    # Yahoo Finance mewajibkan "User-Agent" agar tidak diblokir
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
@@ -31,7 +28,6 @@ def get_harga(pair):
         response = requests.get(url, headers=headers)
         data = response.json()
         
-        # Mengambil harga saat ini (regularMarketPrice) dari struktur JSON Yahoo
         result = data['chart']['result'][0]
         current_price = result['meta']['regularMarketPrice']
         
@@ -45,7 +41,6 @@ def get_harga(pair):
         })
         
     except Exception as e:
-        # Menampilkan detail error jika gagal agar mudah dilacak
         return jsonify({"error": f"Gagal mengambil data dari Yahoo"})
 
 if __name__ == '__main__':
